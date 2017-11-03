2851aed65811465d4792c0c78ae01d9fbf801ad8
RaptorCentauri <dave.leo.shilander@icloud.com>
2017-11-03 02:00:54 -0500

    "added error handling"

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,89 +1,25 @@
-
-
+//Variable Declarations
+var color;
+var type;
+var region;
 var pokemon = {};
 var colorArray = [];
 var	typeArray = [];
 var	regionArray = [];
 var matchesArray = [];
+var resultsArr = [];
+var colorChoices = [];
+var typeChoices = [];
+var regionChoices = [];
 var ajaxColor = $.Deferred();
 var ajaxType = $.Deferred();
 var ajaxRegion = $.Deferred();
 var ajaxResult = $.Deferred();
-var resultsArr = [];
-var color;
-var type;
-var region;
-var colorChoices = [];
-var typeChoices = [];
-var regionChoices = [];
-
-	console.log(`colors available`);
-
-		$.ajax({
-			url: "https://pokeapi.co/api/v2/pokemon-color/",
-			method: "GET"
-		}).done(function(data) {
-			// console.log(data);
-			for (i=0; i<data.results.length; i++ ){
-				colorChoices.push(data.results[i].name);
-			}
-			console.log(colorChoices);
-		});
-
-//====================================================
-		console.log(`types available`);
-
-	$.ajax({
-		url: "https://pokeapi.co/api/v2/type/",
-		method: "GET"
-	}).done(function(data) {
-		// console.log(data);
-			for (i=0; i<data.results.length; i++ ){
-				typeChoices.push(data.results[i].name);
-			}
-		console.log(typeChoices);
-	});
 
 
-//======================================================
+//FUNCTION DECLARATIONS
 
-
-	$.ajax({
-		url: "https://pokeapi.co/api/v2/pokedex/",
-		method: "GET"
-	}).done(function(data) {
-		// console.log(data);
-			for (i=0; i<data.results.length; i++ ){
-				regionChoices.push(data.results[i].name);
-			}
-		console.log(regionChoices);
-	});
-
-
-
-
-
-function simpleEntry(i, _name, _image, _num){
-	// console.log(i);
-	pokemon[i]={
-		name: _name,
-		image: _image,
-		number: _num,
-	}
-}
-
-function initalize() {
-	pokemon = {};
-	// $("#results").html(originalResultsHTML);
-	ajaxColor = $.Deferred();
-	ajaxType = $.Deferred();
-	ajaxRegion = $.Deferred();
-	colorArray = [];
-	typeArray = [];
-	regionArray = [];
-	matchesArray = [];
-}
-
+//Search by specific color
 function colorSearch(color){
 
 	if (!color){
@@ -102,10 +38,13 @@ function colorSearch(color){
 			}
 			console.log(colorArray);
 			ajaxColor.resolve();
-		});
+		}).fail(function(){
+			$("#results").html("The API is not responding");
+		});;
 	}
 }
 
+//Search by specific type
 function typeSearch(type){
 
 	if (!type){
@@ -123,10 +62,13 @@ function typeSearch(type){
 		}
 		console.log(typeArray);
 		ajaxType.resolve();
-		});
+		}).fail(function(){
+			$("#results").html("The API is not responding");
+		});;
 	}
 }
 
+//Search by specific region (using pokedex api)
 function regionSearch(region){
 
 	if (!region){
@@ -144,10 +86,47 @@ function regionSearch(region){
 		}
 		// console.log(regionArray);
 		ajaxRegion.resolve();
-	});	
+	}).fail(function(){
+		$("#results").html("The API is not responding");
+	});;	
 	}
 }
 
+//Create each Pokemon result
+function simpleEntry(i, _name, _image, _num){
+	// console.log(i);
+	pokemon[i]={
+		name: _name,
+		image: _image,
+		number: _num,
+	}
+}
+
+//Dynamically Add Results to Page 
+function buildList(_name, _image, _num){
+	var miniDex = $(`<button class="miniResult" data-name=${_name}>`);
+	miniDex.append(`<img src="${_image}" class="pokemon-img">`);
+	var miniInfo = $(`<div class="miniInfo">`);
+	miniInfo.append(`<h3 class="pokeName">${_name}</h3>`);
+	miniInfo.append(`<h3 class="pokeNumber">${_num}<span></h3>`);
+	miniDex.append(miniInfo);
+	miniDex.append(`<img src="assets/images/pokeball.png" class="pokeball-img">`);
+	$("#results").append(miniDex);
+}
+
+//Reset variables
+function initalize() {
+	pokemon = {};
+	ajaxColor = $.Deferred();
+	ajaxType = $.Deferred();
+	ajaxRegion = $.Deferred();
+	colorArray = [];
+	typeArray = [];
+	regionArray = [];
+	matchesArray = [];
+}
+
+//Determines which search pattern to run based on given parameters
 function searchToRun(colorArray, typeArray, regionArray){
 
 	if (colorArray.length == 0 && typeArray.length == 0){
@@ -186,157 +165,222 @@ function searchToRun(colorArray, typeArray, regionArray){
 	}
 }
 
+function validateSearches(_color, _type, _region){
+	console.log(colorChoices);
+	// console.log(_color);
+
+	console.log(typeChoices);
+
+	console.log(regionChoices);
+
+	if (!colorChoices.includes(_color) && _color != null){
+		console.log('NOT A VALID COLOR!');
+		return false;
+	}
+
+	else if (!typeChoices.includes(_type) && _type != null){
+		console.log('NOT A VALID TYPE!');
+		return false;
+
+	}
+
+	else if (!regionChoices.includes(_region) && _region != null){
+		console.log('NOT A VALID REGION!');
+		return false;
+	}else{
+		return true;
+	}
+}
+
+//Get Color Choices from API
+$.ajax({
+	url: "https://pokeapi.co/api/v2/pokemon-color/",
+	method: "GET"
+}).done(function(data) {
+	// console.log(data);
+	for (i=0; i<data.results.length; i++ ){
+		colorChoices.push(data.results[i].name);
+	}
+	console.log(colorChoices);
+}).fail(function(){
+
+	$("#results").html("The API is not responding");
+});
+
+//Get Type Choices from API
+$.ajax({
+	url: "https://pokeapi.co/api/v2/type/",
+	method: "GET"
+}).done(function(data) {
+		for (i=0; i<data.results.length; i++ ){
+			typeChoices.push(data.results[i].name);
+		}
+}).fail(function(){
+
+	$("#results").html("The API is not responding");
+});;
+
+//Get Pokedex Choices from API
+$.ajax({
+	url: "https://pokeapi.co/api/v2/pokedex/",
+	method: "GET"
+}).done(function(data) {
+		for (i=0; i<data.results.length; i++ ){
+			regionChoices.push(data.results[i].name);
+		}
+}).fail(function(){
+
+	$("#results").html("The API is not responding");
+});;
+
 
 $(document).ready(function() {
-   var originalResultsHTML = $("#results").html();
+	var originalResultsHTML = $("#results").html();
 
-   $(".color-button").on("click", function(){
-   		$("#results").empty();
-   		console.log(`color button works`);
-   		// console.log(colorChoices);
+	//Puts color choices on screen
+	$(".color-button").on("click", function(){
+		$("#results").empty();
+		for (var i = 0; i < colorChoices.length; i++){
+			$("#results").append(`<button class="color-selection" data-color="${colorChoices[i]}">${colorChoices[i]}</button>`);
+		}
+	});
 
-   		for (var i = 0; i < colorChoices.length; i++){
-			$("#results").append(`<button class="color-selection" data-color=${colorChoices[i]} style="color: ${colorChoices[i]}">${colorChoices[i]}</button>`);
-   		}
-
-   });
-
-    $(".type-button").on("click", function(){
-    	$("#results").empty();
-   		console.log(`type button works`);
-   		// console.log(colorChoices);
-
-   		for (var i = 0; i < typeChoices.length; i++){
-			$("#results").append(`<button class="type-selection" data-type="${typeChoices[i]}">${typeChoices[i]}</button>`);
-   		}
-   });
-
-    $(".region-button").on("click", function(){
-    	$("#results").empty();
-   		console.log(`region button works`);
-   		// console.log(colorChoices);
-
-   		for (var i = 0; i < regionChoices.length; i++){
-			$("#results").append(`<button class="region-selection" data-region="${regionChoices[i]}">${regionChoices[i]}</button>`);
-   		}
-   });
-
-   $(document.body).on("click", ".color-selection", function(){
-   		color = $(this).data("color");
-   		console.log(color);
-
-   		$(".color-button").html(color);
-   		// return color;
-   });
-
-  	$(document.body).on("click", ".type-selection", function(){
-   		type = $(this).data("type");
-   		console.log(type);
-   		$(".type-button").html(type);
-   });
-
-
-  	$(document.body).on("click", ".region-selection", function(){
-   		region = $(this).data("region");
-   		console.log(region);
-   		$(".region-button").html(region);
-   });
-
-
-  	$(document.body).on("click", ".miniResult", function(){
-
-  	$.ajax({
-        url: "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + $(this).data("name"),
-        method: 'GET'
-    }).done(function(response) {
-    	console.log(response);
-      var imageUrl = response.data.image_original_url;
-      console.log(imageUrl)
-      $("#results").empty();
-      $("#results").append(`<img src="${imageUrl}">`);
-    });
-
-
-
-   		// region = $(this).data("region");
-   		// console.log(region);
-   		// $(".region-button").html(region);
-   });
-
-	$(document.body).on("click", "#search-button", function() {
+	//Puts type choices on screen
+	$(".type-button").on("click", function(){
 		$("#results").empty();
 
-		initalize();
+		for (var i = 0; i < typeChoices.length; i++){
+			$("#results").append(`<button class="type-selection" data-type="${typeChoices[i]}">${typeChoices[i]}</button>`);
+		}
+	});
 
-		console.log("--------BEGIN SEARCH-----------");
-		// var pokemon = $("#pokemon").val().trim(); //if searching single pokemon data
+	//Puts region choices on screen
+	$(".region-button").on("click", function(){
+		$("#results").empty();
+		for (var i = 0; i < regionChoices.length; i++){
+			$("#results").append(`<button class="region-selection" data-region="${regionChoices[i]}">${regionChoices[i]}</button>`);
+		}
+	});
 
-		colorSearch(color);
+	//Sets color choice
+	$(document.body).on("click", ".color-selection", function(){
+		color = $(this).data("color");
+		$(".color-button").html(color);
+	});
 
-		typeSearch(type);
+	//Sets type choice
+	$(document.body).on("click", ".type-selection", function(){
+	   		type = $(this).data("type");
+	   		console.log(type);
+	   		$(".type-button").html(type);
+	});
 
-		regionSearch(region);
+	//Sets region choice
+	$(document.body).on("click", ".region-selection", function(){
+		region = $(this).data("region");
+		console.log(region);
+		$(".region-button").html(region);
+	});
 
-		console.log(`color= ` + color);
-		console.log(`type= ` + type);
-		console.log(`region= ` + region);
+	//Displays Gif related to the pokemon (NOTE!!! This is using the random generator from Giphy, because of this, results may seem off topic at times.)
+	$(document.body).on("click", ".miniResult", function(){
 
-		// need to delay intersection until its done running....
-		var findMatches = $.when(ajaxColor,ajaxRegion,ajaxType);
+		  	$.ajax({
+		        url: "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + $(this).data("name"),
+		        method: 'GET'
+		    }).done(function(response) {
+		    	console.log(response);
+		      var imageUrl = response.data.image_original_url;
+		      console.log(imageUrl)
+		      $("#results").empty();
+		      $("#results").append(`<img src="${imageUrl}">`);
+		    });
 
 
-		findMatches.done(function() {
+
+		   		// region = $(this).data("region");
+		   		// console.log(region);
+		   		// $(".region-button").html(region);
+	});
+
+	//Runs the search
+	$(document.body).on("click", "#search-button", function() {
+
+		if(!validateSearches(color, type, region)){
+			console.log('INVALID PARAMETERS');
+			$("#results").empty();
+			// $("#results").append("WORK NOW");
+			$("#results").css("background-image", "url('assets/images/missingNo.png')");
+			// $("#results").html('<img src="./assets/images/missingNo.png"');
+		}
+
+		else{
+
+			$("#results").empty();
+
+			initalize();
+
+			console.log("--------BEGIN SEARCH-----------");
+			$("#results").html("Searching for Matches");
+			// var pokemon = $("#pokemon").val().trim(); //if searching single pokemon data
+
+			colorSearch(color);
+
+			typeSearch(type);
+
+			regionSearch(region);
+
+			console.log(`color= ` + color);
+			console.log(`type= ` + type);
+			console.log(`region= ` + region);
+
+			// need to delay intersection until its done running....
+			var findMatches = $.when(ajaxColor,ajaxRegion,ajaxType);
 
 
-			searchToRun(colorArray, typeArray, regionArray);
+			findMatches.done(function() {
 
-			console.log(`Matches on next line meet all criteria!!!!`);
-			console.log(matchesArray);
-			
-			var promises = [];
 
-			for (var i = 0; i < matchesArray.length; i++) {
-				var request = 	$.ajax({
-									url: "https://pokeapi.co/api/v2/pokemon/" + matchesArray[i],
-									method: "GET"
-								}).done(function(data) {
-									// console.log(data);
-									resultsArr.push({name: data.name, image: data.sprites.front_default, num: data.id});					
-								});
+				searchToRun(colorArray, typeArray, regionArray);
 
-				promises.push(request);
-			}
+				console.log(`Matches on next line meet all criteria!!!!`);
+				console.log(matchesArray);
+				
+				var promises = [];
 
-			$.when.apply(null, promises).done(function(){
-				console.log('all done');
-				console.log(resultsArr);
+				for (var i = 0; i < matchesArray.length; i++) {
+					var request = 	$.ajax({
+										url: "https://pokeapi.co/api/v2/pokemon/" + matchesArray[i],
+										method: "GET"
+									}).done(function(data) {
+										// console.log(data);
+										resultsArr.push({name: data.name, image: data.sprites.front_default, num: data.id});					
+									}).fail(function(){
+										$("#results").html("The API is not responding");
+									});;
 
-				for (i=0; i<resultsArr.length; i++){
-					simpleEntry(i, resultsArr[i].name, resultsArr[i].image, resultsArr[i].num);
-					console.log(pokemon[i]);
+					promises.push(request);
 				}
 
+				$.when.apply(null, promises).done(function(){
+					
+					$("#results").empty();
+					console.log('all done');
+					console.log(resultsArr);
 
-				for (var i = 0; i < resultsArr.length; i++){
-					var miniDex = $(`<button class="miniResult" data-name=${pokemon[i].name}>`);
-					miniDex.append(`<img src="${pokemon[i].image}" class="pokemon-img">`);
-					var miniInfo = $(`<div class="miniInfo">`);
-					miniInfo.append(`<h3><span class="pokeName"><span></h3>`);
-					miniInfo.append(`<h3><span class="pokeNumber"><span></h3>`);
-					miniDex.append(miniInfo);
-					miniDex.append(`<img src="assets/images/pokeball.png" class="pokeball-img">`);
-					$("#results").append(miniDex);
-				}
+					for (i=0; i<resultsArr.length; i++){
 
-				$(".pokeName").each(function(i){
-					$(this).html(pokemon[i].name);
-				})
+						simpleEntry(i, resultsArr[i].name, resultsArr[i].image, resultsArr[i].num);
+						console.log(pokemon[i]);
+					}
 
-				$(".pokeNumber").each(function(i){
-					$(this).html(pokemon[i].number);
-				})	
+					for (var i = 0; i < resultsArr.length; i++){
 
+						buildList(pokemon[i].name, pokemon[i].image, pokemon[i].number);
+					}
+				});
 			});
-		});
-	});			
+
+		}
+	});
 })
